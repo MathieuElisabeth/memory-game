cc5b42d0b30a385443062ee89cc40635c4dfc619
MathieuElisabeth <75541968+MathieuElisabeth@users.noreply.github.com>
2022-10-13 15:06:00 +0200

    Update mangas.tsx

--- src/mangas.tsx
+++ src/mangas.tsx
@@ -4,7 +4,7 @@ export const mangas = [
   { id: 3, name: "bleach", image: "https://upload.wikimedia.org/wikipedia/fr/8/86/Bleach_%28manga%29_Logo.svg" },
   { id: 4, name: "dragonball", image: "https://upload.wikimedia.org/wikipedia/fr/f/f2/Dragon_Ball_Z_Logo.svg" },
   { id: 5, name: "hunterxhunter", image: "https://upload.wikimedia.org/wikipedia/commons/c/c8/Hunter_x_hunter.png" },
-  { id: 6, name: "myheroacademia", image: "https://lageekettesportive.com/wp-content/uploads/2019/03/boku_no_hero_academia-1.png" },
+  { id: 6, name: "myheroacademia", image: "https://upload.wikimedia.org/wikipedia/fr/0/0e/My_Hero_Academia_logo_fr.png" },
   { id: 7, name: "fullmetalalchemist", image: "https://upload.wikimedia.org/wikipedia/fr/f/f9/FMA_Brotherhood.png" },
   { id: 8, name: "deathnote", image: "https://upload.wikimedia.org/wikipedia/fr/b/b2/Death_Note_Logo.svg" }
 ];
